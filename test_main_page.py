d77e80d21af13c3bec99a6dd811a0f0e2d37b704
Antova1 <antonovavale22@gmail.com>
2023-02-05 13:19:19 +0100

    main and base pages were added

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
-from selenium.webdriver.common.by import By
-import time 
+from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    time.sleep(30)
-    login_link.click()
-    time.sleep(30)
+    page = MainPage(browser, link) 
+    page.open()                      
+    page.go_to_login_page()
+    
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()    
